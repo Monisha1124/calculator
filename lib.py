aafdb9f7e9ec9ca25bfc65ed1a8494b6147c71ae
Monisha1124 <moniharini07@gmail.com>
2026-08-05 22:02:25 +0530

    Add total_books

---
+++ lib.py
@@ -0,0 +1,12 @@
+def total_books(shelves):
+    return sum(shelves)
+
+def average_books(shelves):
+    return sum(shelves) / len(shelves)
+
+def max_shelf(shelves):
+    return max(shelves)
+
+if __name__ == "__main__":
+    shelves = [12, 8, 15]
+    print("Total:", total_books(shelves))
